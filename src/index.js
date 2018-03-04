bef55c1e26027ff52524c0f34aaff9f1ec524828
Bernardo <bernactkj@gmail.com>
2018-03-03 23:25:59 -0300

    Removing unnecessary code;

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-
-    constructor(props){
-        super(props);
-    }
-
     render(){
         return(
             <h1>
